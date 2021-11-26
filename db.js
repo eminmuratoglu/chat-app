2f3871482a9b38270fd8e5a4410c215b1854fcfc
Emin Muratoglu <eminm@Emins-MacBook-Air.local>
2021-11-26 03:34:00 -0500

    cleanup

--- db.js
+++ db.js
@@ -1,14 +1,5 @@
 const { Pool } = require('pg');
 
-// const { Client } = require('pg');
-// const client = new Client({
-//   host: "localhost", -> process.env.DB_HOST,
-//   user: "postgres", -> process.env.DB_USER
-//   port: 5432,
-//   password: '', -> process.env.DB_PASSWORD, (the password I made at installation)
-//   database: "postgres" -> process.env.DB_NAME,
-// });
-
 const pool = new Pool({
 	host: 'localhost',
 	user: process.env.DB_PG_USER,
@@ -25,4 +16,13 @@ module.exports = pool;
 // 	id SERIAL NOT NULL PRIMARY KEY,
 // 	username VARCHAR(25) NOT NULL,
 // 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+// )
+
+// CREATE TABLE messages(
+// 	id SERIAL NOT NULL,
+// 	user_id SERIAL,
+// 	text VARCHAR(250),
+// 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+// 	PRIMARY KEY(id),
+// 	FOREIGN KEY (user_id) REFERENCES users(id)
 // )
