4b6d1b0db9d73b8a1ff398e76fa3ec90ff84487d
Emin Muratoglu <eminm@Emins-MacBook-Air.local>
2021-12-14 02:41:22 -0500

    users route separated

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-// const axios = require('axios');
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
 const pool = require('./db');
-
+const cookieParser = require('cookie-parser');
 const app = express();
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/api/messages', async (req, res) => {
 	try {
@@ -27,24 +29,12 @@ app.post('/api/messages', async (req, res) => {
 		console.error(err.message);
 	}
 });
-app.get('/api/users', async (req, res) => {
-	try {
-		const allUsers = await pool.query('SELECT * FROM users');
-		res.send(allUsers.rows);
-	} catch (err) {
-		console.error(err.message);
-	}
-});
-app.post('/api/users', async (req, res) => {
-	try {
-		const { text } = req.body;
-		const newUser = await pool.query('INSERT INTO users (text) VALUES($1) RETURNING *', [ text ]);
-		res.send(newUser.rows[0]);
-	} catch (err) {
-		console.error(err.message);
-	}
-});
-// app.get('/api/users/{id}', (req, res) => {});
+// While creating the message, I need to specify user_id of which user it belongs to (which user logged in)
+
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
+
+////
 
 app.use('/', express.static(path.join(__dirname, 'client/build')));
 
