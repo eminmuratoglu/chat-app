9bfb80a0b464b851ee1ad9cf6317b009f8d67aaf
Emin Muratoglu <eminm@Emins-MacBook-Air.local>
2021-12-21 04:34:52 -0500

    socketio added

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const http = require('http');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
 const usersRouter = require('./routes/users');
 const messagesRouter = require('./routes/messages');
 const authRouter = require('./routes/auth');
@@ -24,8 +28,13 @@ app.use('/api/users', usersRouter);
 app.use('/api/messages', messagesRouter);
 app.use('/api/auth', authRouter);
 
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	io.emit('welcome', 'hello, this is socket server');
+});
+
 ////
 
 app.use('/', express.static(path.join(__dirname, 'client/build')));
 
-app.listen(3000);
+server.listen(process.env.PORT || 3000);
