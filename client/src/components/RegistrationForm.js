2ed4e3e4eff998ac87ab44f929b64aebeae6f901
eminmuratoglu <eminm.6161@gmail.com>
2021-12-22 04:45:53 -0500

    already have an account? login / navlink added

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -1,21 +1,50 @@
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 import './RegistrationForm.css';
 
 function RegistrationForm() {
-	const handleSubmit = async (e) => {
+	const [ username, setUsername ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	const handleRegister = async (e) => {
 		e.preventDefault();
+		try {
+			const res = await axios.post('/api/users', { username, password });
+			console.log(res.data);
+			return res;
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
 		<div className="registrationContainer">
 			<h2>Registration</h2>
-			<form className="registrationForm">
+			<form className="registrationForm" onSubmit={handleRegister}>
 				<label>Username</label>
-				<input type="text" />
+				<input
+					type="text"
+					placeholder="username"
+					name="username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
 				<label>Password</label>
-				<input type="password" />
+				<input
+					type="password"
+					placeholder="password"
+					name="username"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
 				<button type="submit">Register</button>
+				<p>
+					Already have an account?{' '}
+					<NavLink exact to="/login">
+						Log in
+					</NavLink>
+				</p>
 			</form>
 		</div>
 	);
