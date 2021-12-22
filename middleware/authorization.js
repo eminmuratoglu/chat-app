27140a8d8297eac4338443654bdb31ee64123f15
Emin Muratoglu <eminm@Emins-MacBook-Air.local>
2021-12-22 04:45:53 -0500

    already have an account? login / navlink added

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonwebtoken');
+const { verify } = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
 	const authHeader = req.headers['authorization'];
 	const token = authHeader && authHeader.split(' ')[1];
-	if (token === null) return res.status(401).json({ error: 'Token error!' });
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+	if (!token) return res.status(401).json({ error: 'Token (authentication) error!' });
+	verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
 		if (error) return res.status(403).json({ error: error.message });
 		req.user = user;
 		next();
