081353313cfeffce5a6e1c675a5dcccccc5abb79
Emin Muratoglu <eminm@Emins-MacBook-Air.local>
2021-12-22 04:42:34 -0500

    comment deleted

--- routes/messages.js
+++ routes/messages.js
@@ -4,7 +4,7 @@ const { authenticateToken } = require('../middleware/authorization');
 
 const router = express.Router();
 
-app.get('/', authenticateToken, async (_req, res) => {
+router.get('/', authenticateToken, async (_req, res) => {
 	try {
 		const allMessages = await pool.query('SELECT * FROM messages');
 		res.send(allMessages.rows);
@@ -13,16 +13,17 @@ app.get('/', authenticateToken, async (_req, res) => {
 	}
 });
 
-app.post('/', authenticateToken, async (req, res) => {
+router.post('/', authenticateToken, async (req, res) => {
 	try {
-		const { text } = req.body;
-		const newMsg = await pool.query('INSERT INTO messages (text) VALUES($1) RETURNING *', [ text ]);
+		const { text, user_id } = req.body;
+		const newMsg = await pool.query('INSERT INTO messages (text, user_id) VALUES($1, $2) RETURNING *', [
+			text,
+			user_id
+		]);
 		res.send(newMsg.rows[0]);
 	} catch (err) {
 		console.error(err.message);
 	}
 });
-
-// While creating the message, I need to specify user_id of (which) user it belongs to (which user logged in)
 
 module.exports = router;
