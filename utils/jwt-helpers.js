184f58d706e54defe2910766e8d8049ae4ac71ab
Emin Muratoglu <eminm@Emins-MacBook-Air.local>
2021-12-22 04:41:13 -0500

    access_token expires in 5s to test

--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function generateTokens({ user_id, username }) {
 	const user = { user_id, username };
-	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5s' });
 	const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '14d' });
 	return { accessToken, refreshToken };
 }
