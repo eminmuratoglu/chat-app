860ca8c94c8100b4806024ff56950b5ff234644a
eminmuratoglu <eminm.6161@gmail.com>
2022-01-18 02:23:17 -0500

    db

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE chat_app;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+ 	created_at TIMESTAMPTZ DEFAULT NOW()
+ );
+
+ CREATE TABLE messages(
+	id SERIAL NOT NULL,
+	user_id SERIAL NOT NULL,
+	username VARCHAR(50) NOT NULL,
+	text VARCHAR(250),
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	PRIMARY KEY(id)
+);
