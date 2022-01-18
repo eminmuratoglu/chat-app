56bd9e0f972e4e0f8879584953416ae5dc002013
eminmuratoglu <eminm.6161@gmail.com>
2022-01-18 02:40:16 -0500

    db updated according to NODE_ENV

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-// 	user: process.env.DB_PG_USER,
-// 	password: process.env.DB_PG_PASSWORD,
-// 	host: process.env.DB_PG_HOST,
-// 	database: process.env.DB_PG_DATABASE,
-// 	port: process.env.DB_PG_PORT
-// });
+const devConfig = {
+	user: process.env.DB_PG_USER,
+	password: process.env.DB_PG_PASSWORD,
+	host: process.env.DB_PG_HOST,
+	database: process.env.DB_PG_DATABASE,
+	port: process.env.DB_PG_PORT
+};
 
-const pool = new Pool({
+const productionConfig = {
 	connectionString: process.env.DATABASE_URL
-});
+};
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? productionConfig : devConfig);
 
 module.exports = pool;
