85f49864d04924fc4807683715cb56dad618f4e7
eminmuratoglu <eminm.6161@gmail.com>
2022-01-18 20:45:36 -0500

    pool.connect()

--- db.js
+++ db.js
@@ -1,33 +1,22 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-// 	user: process.env.DB_PG_USER,
-// 	password: process.env.DB_PG_PASSWORD,
-// 	host: process.env.DB_PG_HOST,
-// 	database: process.env.DB_PG_DATABASE,
-// 	port: process.env.DB_PG_PORT
-// });
+const devConfig = {
+	user: process.env.DB_PG_USER,
+	password: process.env.DB_PG_PASSWORD,
+	host: process.env.DB_PG_HOST,
+	database: process.env.DB_PG_DATABASE,
+	port: process.env.DB_PG_PORT
+};
 
-const pool = new Pool({
+const productionConfig = {
 	connectionString: process.env.DATABASE_URL,
-	port: 5432,
 	ssl: {
 		rejectUnauthorized: false
 	}
-});
+};
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? productionConfig : devConfig);
+
+pool.connect();
 
 module.exports = pool;
-
-// const devConfig = {
-// 	user: process.env.DB_PG_USER,
-// 	password: process.env.DB_PG_PASSWORD,
-// 	host: process.env.DB_PG_HOST,
-// 	database: process.env.DB_PG_DATABASE,
-// 	port: process.env.DB_PG_PORT
-// };
-
-// const productionConfig = {
-// 	connectionString: process.env.DATABASE_URL
-// };
-
-// const pool = new Pool(process.env.NODE_ENV === 'production' ? productionConfig : devConfig);
