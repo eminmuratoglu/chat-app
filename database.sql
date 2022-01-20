83831cf6dbf1a7315d4316b9dc75f6064baa82fb
eminmuratoglu <eminm.6161@gmail.com>
2022-01-20 02:47:04 -0500

    varchar 255

--- database.sql
+++ database.sql
@@ -2,16 +2,16 @@
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
-	password VARCHAR(50) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
  	created_at TIMESTAMPTZ DEFAULT NOW()
  );
 
 CREATE TABLE messages(
 	id SERIAL NOT NULL,
 	user_id SERIAL NOT NULL,
-	username VARCHAR(50) NOT NULL,
-	text VARCHAR(250),
+	username VARCHAR(255) NOT NULL,
+	text VARCHAR(255),
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	PRIMARY KEY(id)
 );
